bbfd63bc5e1e4aba2eded61516c72c9559c73136
Akaba_nSchool <orih.9619@outlook.jp>
2020-10-27 15:08:33 +0900

    InitCommit

---
+++ TypingMaster/Assets/Scripts/System/AppDefine.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* ゲームの基底クラス */
+public class AppDefine : MonoBehaviour
+{
+    // 最高FPSの定義(constが定義化)
+    public const int MAX_FPS = 60;
+
+    // シーンの状態
+    public enum SCENE_STATE {
+        START,
+        PLAY,
+        CLEAR,
+        CHANGE_WAIT
+    };
+
+    // サウンドの種類
+    public enum SOUND_TYPE {
+        BGM,
+        SE,
+        VOICE
+    };
+}
---
+++ TypingMaster/Assets/Scripts/System/Debug/DebugManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+////// デバッグ(開発)用クラス //////
+public class DebugManager : MonoBehaviour {
+
+    [SerializeField] GameObject fpsRenderSystem = null;
+
+    public bool LogRender = false;
+    public bool FpsRender = false;
+
+    public void SetDebugLog(string text) {
+
+        if (LogRender == true) {
+
+            Debug.Log(text);
+        }
+    }
+
+    public void Update() {
+
+        if (FpsRender == true) {
+
+            if (fpsRenderSystem.activeSelf != FpsRender) {
+
+                fpsRenderSystem.SetActive(FpsRender);
+            }
+        }
+    }
+}
