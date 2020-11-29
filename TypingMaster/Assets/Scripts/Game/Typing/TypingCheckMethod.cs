cc9a9a228237b5749a3e9583ca3d6bbd2b270bf5
Akaba_n <orih.9619@outlook.jp>
2020-11-29 22:26:29 +0900

    文章サンプル追加 + タイピング部分少しだけ

--- TypingMaster/Assets/Scripts/Game/Typing/TypingCheckMethod.cs
+++ TypingMaster/Assets/Scripts/Game/Typing/TypingCheckMethod.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Game;
 
 public class TypingCheckMethod : TypingDirector {
     
@@ -21,8 +22,34 @@ public class TypingCheckMethod : TypingDirector {
 
             // timeQueueに入っている時刻が最後に判定があった時刻より早かったら整合性が無いのでスキップ
             if (keyDownTime <= lastJudgeTime) { continue; }
+
+            // EscKeyの場合メニュー画面を開く(中身は後で実装)
+            if (kc == KeyCode.Escape) {
+
+                ///// メニュー画面を開くメソッドを作る /////
+                return;
+            }
         }
 
         
+    }
+
+    private void MisTypeCheck(KeyCode kc) {
+
+        bool isMistype = true;
+        string str = "";
+
+        // 全てのvalid(有効)なセンテンスに対してチェックする
+        for (var i = 0; i < sentenceTyping[index].Count; ++i) {
+
+            // validの場合
+            if (sentenceValid[index][i]) {
+
+                int j = sentenceIndex[index][i];
+            }
+
+            
+        
+        }
     }
 }
